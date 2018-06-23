6fd1acfe5e7487f0bd857fc216f131560f5b20ba
Damparo <95daniele26@gmail.com>
2018-06-23 18:14:51 +0200

    Set required packages erathostenes and primes
    Copied into ./sophie erathostenes' files

---
+++ src/erathostenes/Token.java
@@ -0,0 +1,31 @@
+package primes.erathostenes;
+
+import java.math.BigInteger;
+
+public class Token {
+
+	private BigInteger num;
+
+		//constructors
+	public Token() {
+		System.out.println("new erathostenes Token");
+		
+		this.num = BigInteger.ZERO;
+	}
+
+		// setters
+	public	void Set(BigInteger n) {
+		this.num = n;
+	}
+	
+		// getters
+	public BigInteger value() {
+		return this.num;
+	}
+
+	public BigInteger value2() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/primes/Item.java
@@ -0,0 +1,53 @@
+package primes;
+
+import java.math.BigInteger;
+
+public abstract class Item<T> {
+	public Item<T> next;
+
+		//constructors
+	public Item () {
+		this.set(null);
+	}
+
+	public Item (Item<T> tail) {
+		this.set(tail);
+	}
+
+		//setters
+
+	public void set ( Item<T> list) {
+		this.next = list;
+	}
+
+		//getters
+	int length() {
+		Item<T> lista = this;
+		int i = 0;
+	
+		while((lista != null)) {
+			i++;
+			lista = lista. next;
+		}
+		
+		return i;
+	}
+	
+	public	Item<T> next() {
+		return next;
+	}
+	
+	int rlength() {
+		if (this.next==null)
+			return 1;
+		else
+			return (next.rlength()+1);
+	}
+
+	abstract public void print();
+		
+	abstract public T get();
+		
+	abstract public BigInteger value();
+
+}
